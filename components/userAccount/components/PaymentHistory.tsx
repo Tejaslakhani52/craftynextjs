b1ca764e6ddd0c100dc4dfdad4f713ff58213f47
Tejas Lakhani <tejaslakhani52@gmail.com.com>
2024-01-10 12:19:02 +0530

    check

--- components/userAccount/components/PaymentHistory.tsx
+++ components/userAccount/components/PaymentHistory.tsx
@@ -6,8 +6,7 @@ export default function PaymentHistory({ userSubscription }: any) {
   return (
     <>
       <Head>
-        <Link
-          prefetch={false}
+        <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
           integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
